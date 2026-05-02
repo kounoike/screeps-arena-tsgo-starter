5d949e331ee6c97ba293d7c65353b7815e61784a
KOUNOIKE Yuusuke <kounoike.yuusuke@gmail.com>
2026-05-03 04:31:34 +0900

    update season 3 type definitions

--- src/season_3/er/main/simple-run.ts
+++ src/season_3/er/main/simple-run.ts
@@ -1,4 +1,4 @@
-import { Creep, Flag } from 'game/prototypes'
+import { EscortCreep, Flag } from 'game/prototypes'
 import { getObjectsByPrototype, getTicks } from 'game/utils'
 import { Logger } from '@/common/logger.js'
 import { run as runToFlag } from '../action/run.js'
@@ -6,7 +6,7 @@ import { run as runToFlag } from '../action/run.js'
 const logger = new Logger('simple-run', true)
 
 export function loop(): void {
-  const myCreeps = getObjectsByPrototype(Creep).filter(creep => creep.my)
+  const myCreeps = getObjectsByPrototype(EscortCreep).filter(creep => creep.my)
   const myFlag = getObjectsByPrototype(Flag).find(flag => flag.my)
   if (!myFlag) {
     return
